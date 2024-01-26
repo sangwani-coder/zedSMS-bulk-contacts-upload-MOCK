2cbeab4034ea0cb1f0727b381af1bedab4b52116
sangwani <mightypz2@gmail.com>
2024-01-26 08:48:13 +0200

    Implement file upload and type validation

--- bulkuploads-mock/src/app/upload/upload.component.ts
+++ bulkuploads-mock/src/app/upload/upload.component.ts
@@ -1,19 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-upload',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
     <main class="main">
-      <form action="/">
-        <input type="file" id="myFile" name="filename">
-        <input type="submit">
-      </form>
+      <input type="file" (change)="onFileSelected($event)">
+      <p *ngIf="selectedFile">Selected file: {{ selectedFile.name }}</p>
     </main>
   `,
-  styleUrl: './upload.component.css'
+  styleUrls: ['./upload.component.css']
 })
 export class UploadComponent {
+  selectedFile: File | undefined = undefined;
 
+  onFileSelected(event: any) {
+    const file = event.target?.files?.[0]; // Use optional chaining
+
+    // Validate file type (CSV)
+    if (file.type !== 'text/csv') {
+      console.error('Invalid file type. Please select a CSV file.');
+      return;
+    }
+
+    this.selectedFile = file;
+    console.log('Selected file:', file);
+
+    // Initiate the upload process to the backend
+  }
 }

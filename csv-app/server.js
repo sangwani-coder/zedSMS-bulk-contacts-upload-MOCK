5af9a02af8a1e79f1c343ba45d01e66feb2f6820
sangwani <mightypz2@gmail.com>
2024-01-27 17:45:23 +0200

    Implement inserting data in Mongoose via POST

--- csv-app/server.js
+++ csv-app/server.js
@@ -1,34 +1,24 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
 
 const port = 3333;
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://mightypz2:dev200Connects@cluster0.ifurnju.mongodb.net/?retryWrites=true&w=majority";
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri);
-
-// MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-//   if (err) {
-//     console.error('Error connecting to MongoDB:', err);
-//     return;
-
-//   }else {
-//     console.log("Connected to MongoDB")
-//   }
-const db = client.db("zedsms_contacts_mock");
+const db = client.db("zedsms_mock");
 
 
-async function retrieveData() {
+async function retrieveData(company_name) {
   try {
-    const collection = db.collection("conpanies");
+    const collection = db.collection("contacts");
 
     // Find the first document in the collection
     const data = await collection.findOne(); // Use findOne to retrieve only the first document
-
-    // Find Specific Company data
-    const companyBData = data.companies.find(company => company.name === 'Company A');
-    return companyBData;
+    // const companyBData = data.companies.find(company => company.name === company_name);
+    // console.log(companyBData);
+    return data;
 
   } catch (err) {
     console.error('Error in retrieveData function:', err);
@@ -38,14 +28,24 @@ async function retrieveData() {
   }
 }
 
-
+async function InsertData(data) {
+  try {
+    const collection = db.collection("contacts");
+    const result = await collection.insertOne(data);
+    console.log(`Data inserted successfully with ID: ${result.insertedId}`);
+  } catch (err) {
+    console.error('Error inserting data:', err);
+    throw err; // Re-throw the error to handle it in the endpoint
+  }
+}
 
 // GET endpoint to retrieve the first document
 app.get('/data', async (req, res) => {
   try {
-    const firstDoc = await retrieveData();
-    if (firstDoc) {
-      res.json(firstDoc);
+    const company = req.query.company_name;
+    const result = await retrieveData(company);
+    if (result) {
+      res.json(result);
     } else {
       res.status(404).send('No documents found');
     }
@@ -55,22 +55,16 @@ app.get('/data', async (req, res) => {
   }
 });
 
-// // POST endpoint to retrieve the first document
-// app.get('/data', async (req, res) => {
-//   try {
-//     const data = 
-//     const firstDoc = await retrieveData();
-//     if (firstDoc) {
-//       res.json(firstDoc);
-//     } else {
-//       res.status(404).send('No documents found');
-//     }
-//   } catch (err) {
-//     console.error('Error in GET endpoint:', err);
-//     res.status(500).send('Error retrieving data');
-//   }
-// });
-// });
+// POST endpoint to retrieve the first document
+app.post('/data', bodyParser.json(), async (req, res) => {
+  try {
+    await InsertData(req.body); // Pass the parsed request body to insertData
+    res.status(201).send('Data saved successfully!');
+  } catch (err) {
+    console.error('Error in POST endpoint:', err);
+    res.status(500).send('Error saving data');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);

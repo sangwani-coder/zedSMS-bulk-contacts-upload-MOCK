bacff9bb154ac4b0570fa9d1661c29b0283fac85
sangwani <mightypz2@gmail.com>
2024-01-29 22:31:33 +0200

    create a contactsComponent

---
+++ bulkuploads-mock/src/app/contacts/contacts.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './contacts.component.html',
+  styleUrl: './contacts.component.css'
+})
+export class ContactsComponent {
+
+}
